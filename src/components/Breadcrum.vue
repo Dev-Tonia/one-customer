a010d8ea7f67460bb57ac8942d88f6e0569692f6
Dev-Tonai <ogunwaanthonia@gmail.com>
2023-10-27 16:19:12 +0100

    change the text color of the core value section

--- src/components/Breadcrum.vue
+++ src/components/Breadcrum.vue
@@ -2,7 +2,7 @@
 const { title } = defineProps(["title"]);
 </script>
 <template>
-  <span class="text-xl mx-4 text-[#333] bg-green-500 px-3 rounded-full">{{
+  <span class="text-xl mx-4 text-white bg-green-500 px-3 rounded-full">{{
     title
   }}</span>
 </template>
